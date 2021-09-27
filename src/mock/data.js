ae2af842e4e387ab1d89429d16f4715c8985051a
allyroo <allyse.arucan@gmail.com>
2021-09-27 14:07:33 -0700

    profile

--- src/mock/data.js
+++ src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.png',
+  img: 'portprofile.jpg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
