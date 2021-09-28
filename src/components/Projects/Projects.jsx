add8cc6a01effde88607e7e8c3200fb0db200d6c
allyroo <allyse.arucan@gmail.com>
2021-09-28 04:27:18 -0700

    source code error

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -50,14 +50,10 @@ const Projects = () => {
                         <p className="mb-4">{info2 || ''}</p>
                       </div>
                       <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn text-color-main"
-                      href={repo}
-                        // target="_blank"
-                        // rel="noopener noreferrer"
-                        // className="cta-btn cta-btn--hero"
-                        // href={url || '#!'}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="cta-btn cta-btn--hero"
+                        href={repo}
                       >
                         Source Code
                       </a>
@@ -71,10 +67,51 @@ const Projects = () => {
                         >
                           Source Code
                         </a>
+                        
                       )} */}
                     </div>
                   </Fade>
                 </Col>
+                {/* <Col lg={4} sm={12}>
+                  <Fade
+                    left={isDesktop}
+                    bottom={isMobile}
+                    duration={1000}
+                    delay={500}
+                    distance="30px"
+                  >
+                    <div className="project-wrapper__text">
+                      <h3 className="project-wrapper__text-title">{title || 'Project Title'}</h3>
+                      <div>
+                        <p>
+                          {info ||
+                            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                        </p>
+                        <p className="mb-4">{info2 || ''}</p>
+                      </div>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="cta-btn cta-btn--hero"
+                        href={url || '#!'}
+                      >
+                        See Live
+                      </a>
+
+                      {repo && (
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="cta-btn text-color-main"
+                          href={repo}
+                        >
+                          Source Code
+                        </a>
+                        
+                      )}
+                    </div>
+                  </Fade>
+                </Col> */}
                 <Col lg={8} sm={12}>
                   <Fade
                     right={isDesktop}
